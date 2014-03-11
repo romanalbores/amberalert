a2b430847b0192af88b37fcbfde20212a8965062
romanalbores <romanalbores@gmaill.com>
2014-03-11 12:03:40 -0600

    Catálogos del Sistema

--- protected/views/imagen/_form.php
+++ protected/views/imagen/_form.php
@@ -23,23 +23,13 @@
 
 	<?php echo $form->textFieldRow($model,'publico',array('class'=>'span5')); ?>
 
-	<?php echo $form->textFieldRow($model,'estatus',array('class'=>'span5','maxlength'=>15)); ?>
-
-	<?php echo $form->textFieldRow($model,'registrado_por',array('class'=>'span5')); ?>
-
-	<?php echo $form->textFieldRow($model,'fecha_registro',array('class'=>'span5')); ?>
-
-	<?php echo $form->textFieldRow($model,'modificado_por',array('class'=>'span5')); ?>
-
-	<?php echo $form->textFieldRow($model,'fecha_modificado',array('class'=>'span5')); ?>
-
-	<?php echo $form->textFieldRow($model,'eliminado',array('class'=>'span5')); ?>
+	 
 
 <div class="form-actions">
 	<?php $this->widget('bootstrap.widgets.TbButton', array(
 			'buttonType'=>'submit',
 			'type'=>'primary',
-			'label'=>$model->isNewRecord ? 'Create' : 'Save',
+			'label'=>$model->isNewRecord ? 'Create' : 'Guardar',
 		)); ?>
 </div>
 
