9d1c2a06bdd768e576269c7037194344c191ca4d
romanalbores <romanalbores@gmaill.com>
2014-03-11 13:49:18 -0600

    Testando

--- protected/views/incidencia/create.php
+++ protected/views/incidencia/create.php
@@ -9,36 +9,9 @@ array('label'=>'List Incidencia','url'=>array('index')),
 array('label'=>'Manage Incidencia','url'=>array('admin')),
 );
 ?>
-ping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-pingping
-ping
+
 Este es un comentario de prueba para el commit
+
 <h1>Crear Incidencia</h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model,'modelIncidenciaTiempo' => $modelIncidenciaTiempo)); ?>
